bfb1a7a1b378848caa1a0a6b2e4aa1b493a8682b
[Mariia] <[mahadoro@gmail.com]>
2021-10-24 15:20:46 +0300

    Second commit for Alpha

---
+++ GitPracticeForAlpha/Commit2.java
@@ -0,0 +1,9 @@
+package GitPracticeForAlpha;
+
+public class Commit2 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        System.out.println(b-a);
+    }
+}
