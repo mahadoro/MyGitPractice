c01f468ce6de540169f5610b1bc01a77957c2f83
[Mariia] <[mahadoro@gmail.com]>
2021-10-24 15:33:43 +0300

    Revert "Third commit for Alpha"
    This reverts commit 02c1ea54f40bb9b51cf8166c2c6e4f2f32806c51.

--- GitPracticeForAlpha/Commit3.java
+++
@@ -1,9 +0,0 @@
-package GitPracticeForAlpha;
-
-public class Commit3 {
-    public static void main(String[] args) {
-        int a = 2;
-        int b = 5;
-        System.out.println(a*b);
-    }
-}
