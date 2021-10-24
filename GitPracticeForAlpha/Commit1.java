2957cd0a731980280ed5d6b6a29e2ab8af9685a8
[Mariia] <[mahadoro@gmail.com]>
2021-10-24 15:20:25 +0300

    First commit for Alpha

---
+++ GitPracticeForAlpha/Commit1.java
@@ -0,0 +1,9 @@
+package GitPracticeForAlpha;
+
+public class Commit1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println(a+b);
+    }
+}
